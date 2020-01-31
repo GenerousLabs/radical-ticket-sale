f16fd442c86da591774003bf54993e1abc39b929
Callum Macdonald <git@callum-macdonald.com>
2020-01-31 15:18:10 +0100

    Separate Intro component.

--- frontend/src/scenes/Pledge/Pledge.scene.tsx
+++ frontend/src/scenes/Pledge/Pledge.scene.tsx
@@ -16,6 +16,7 @@ import { useSelector } from "react-redux";
 
 import { AppState } from "../../store";
 import { ExternalLink } from "../../components/ExternalLink";
+import Intro from "./components/Intro.component";
 
 const numbers = {
   min: "€60'000",
@@ -33,6 +34,7 @@ const checks = {
 const Pledge = () => {
   const classes = useStyles();
   const hasToken = useSelector((state: AppState) => state.token.token !== "");
+  const [readIntro, setReadIntro] = useState(false);
   const [name, setName] = useState("");
   const [agreedTerms, setAgreedTerms] = useState(checks);
   const [price, setPrice] = useState(-1);
@@ -46,6 +48,32 @@ const Pledge = () => {
     return null;
   }
 
+  if (!readIntro) {
+    return (
+      <div className={classes.container}>
+        <Intro numbers={numbers} />
+        <Paper className={classes.paper}>
+          <Typography variant="h3">Buy a ticket</Typography>
+          <Typography className={classes.p}>
+            If you're happy with the details above, you can start your ticket
+            purchase now.
+          </Typography>
+          <Button
+            onClick={() => {
+              setReadIntro(true);
+              window.scrollTo(0, 0);
+            }}
+            variant="contained"
+            size="large"
+            color="primary"
+          >
+            Buy a ticket
+          </Button>
+        </Paper>
+      </div>
+    );
+  }
+
   return (
     <div className={classes.container}>
       <Paper className={classes.paper}>
@@ -53,56 +81,8 @@ const Pledge = () => {
         <Typography className={classes.p}>
           Welcome to the Kiez Burn 2020 ticket extravaganza.
         </Typography>
-        <Typography className={classes.p}>
-          We trust you already know what Kiez Burn is. But just in case, here's
-          a few links for you:
-        </Typography>
-        <ul>
-          <li>
-            <ExternalLink href="https://kiezburn.org">
-              The Kiez Burn web site
-            </ExternalLink>
-          </li>
-          <li>
-            <ExternalLink href="https://talk.kiezburn.org/dashboard">
-              The Talk platform where Kiez Burn decisions are discussed
-            </ExternalLink>
-          </li>
-          <li>
-            <ExternalLink href="https://kiezburn.org/home/about/11-principles/">
-              The Kiez Burn 11 principles
-            </ExternalLink>
-          </li>
-        </ul>
-        <Typography className={classes.p}>
-          In this spirit of cocreation and experimentation we're trying
-          something new for 2020.
-        </Typography>
-        <Typography className={classes.p}>
-          Based on our{" "}
-          <ExternalLink href="https://talk.kiezburn.org/g/QFjHmdZ2/2019-archive">
-            financials from 2019
-          </ExternalLink>
-          , a budget of <strong>{numbers.target}</strong> would make for a
-          fantastic Kiez Burn 2020. The bear minimum to make an event happen
-          would be at least {numbers.min}.
-        </Typography>
-        <Typography className={classes.p}>
-          What would you like Kiez Burn's budget to be for 2020?
-        </Typography>
-        <Typography className={classes.p}>
-          You're invited to vote with your wallet. On this page you can pledge
-          to buy a Kiez Burn ticket. The system will record your payment
-          information, and if we collect 1'000 pledges, and the total budget
-          from those pledges is more than our minimum, we'll collect all the
-          money. This sale begins 1 Feb and ends by 21 Feb, or whenever all
-          spots are filled.
-        </Typography>
-        <Typography className={classes.p}>
-          For more information, to ask questions, or to engage in this process,{" "}
-          <ExternalLink href="">click here</ExternalLink>.
-        </Typography>
       </Paper>
+
       <Paper className={classes.paper}>
         <Typography variant="h3">Legal name</Typography>
         <Typography>Enter your legal name</Typography>
@@ -239,7 +219,7 @@ const Pledge = () => {
                 <FormControlLabel
                   value="29800"
                   control={<Radio />}
-                  label="Generous ticket - €148"
+                  label="Generous ticket - €298"
                 />
                 <FormControlLabel
                   value="14800"
@@ -291,7 +271,7 @@ const Pledge = () => {
               </Paper>
 
               <Paper className={classes.paper}>
-                <Typography variant="h3">Card Details</Typography>
+                <Typography variant="h3">Payment</Typography>
                 <Typography>
                   You have chosen to pay €{(price / 100).toFixed(2)}
                 </Typography>
@@ -314,7 +294,7 @@ const Pledge = () => {
   );
 };
 
-const useStyles = makeStyles((theme: Theme) => ({
+export const useStyles = makeStyles((theme: Theme) => ({
   container: {
     maxWidth: 600,
     paddingBottom: 300
---
+++ frontend/src/scenes/Pledge/components/Intro.component.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { Paper, Typography } from "@material-ui/core";
+
+import { ExternalLink } from "../../../components/ExternalLink";
+import { useStyles } from "../Pledge.scene";
+
+type Props = {
+  numbers: {
+    [key: string]: string | number;
+  };
+};
+
+const Intro: React.FC<Props> = props => {
+  const { numbers } = props;
+  const classes = useStyles();
+
+  return (
+    <>
+      <Paper className={classes.paper}>
+        <Typography variant="h3">Welcome</Typography>
+        <Typography className={classes.p}>
+          Welcome to the Kiez Burn 2020 ticket extravaganza.
+        </Typography>
+        <Typography className={classes.p}>
+          We trust you already know what Kiez Burn is. But just in case, here's
+          a few links for you:
+        </Typography>
+        <ul>
+          <li>
+            <ExternalLink href="https://kiezburn.org">
+              The Kiez Burn web site
+            </ExternalLink>
+          </li>
+          <li>
+            <ExternalLink href="https://talk.kiezburn.org/dashboard">
+              The Talk platform where Kiez Burn decisions are discussed
+            </ExternalLink>
+          </li>
+          <li>
+            <ExternalLink href="https://kiezburn.org/home/about/11-principles/">
+              The Kiez Burn 11 principles
+            </ExternalLink>
+          </li>
+        </ul>
+        <Typography className={classes.p}>
+          In this spirit of cocreation and experimentation we're trying
+          something new for 2020.
+        </Typography>
+        <Typography className={classes.p}>
+          Based on our{" "}
+          <ExternalLink href="https://talk.kiezburn.org/g/QFjHmdZ2/2019-archive">
+            financials from 2019
+          </ExternalLink>
+          , a budget of <strong>{numbers.target}</strong> would make for a
+          fantastic Kiez Burn 2020. The bear minimum to make an event happen
+          would be at least {numbers.min}.
+        </Typography>
+        <Typography className={classes.p}>
+          What would you like Kiez Burn's budget to be for 2020?
+        </Typography>
+        <Typography className={classes.p}>
+          You're invited to vote with your wallet. On this page you can pledge
+          to buy a Kiez Burn ticket. The system will record your payment
+          information, and if we collect 1'000 pledges, and the total budget
+          from those pledges is more than our minimum, we'll collect all the
+          money. This sale begins 1 Feb and ends by 21 Feb, or whenever all
+          spots are filled.
+        </Typography>
+        <Typography className={classes.p}>
+          For more information, to ask questions, or to engage in this process,{" "}
+          <ExternalLink href="">click here</ExternalLink>.
+        </Typography>
+      </Paper>
+
+      <Paper className={classes.paper}>
+        <Typography variant="h3">Budget</Typography>
+        <Typography className={classes.p}>
+          We're working on the budget proposals for Kiez Burn 2020, more info
+          will be posted here shortly.
+        </Typography>
+      </Paper>
+    </>
+  );
+};
+
+export default Intro;
